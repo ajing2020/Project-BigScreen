aa4f4f74f7959c9aa39e8ebb86a6c6da2ce1f099
ajing2020 <1277483876@qq.com>
2022-05-26 14:43:55 +0800

    文字错误

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -60,7 +60,7 @@ export const Home = () => {
             </div>
           </div>
           <div className="bordered row3 作案手段">
-            <h2>作案手段分析</h2>
+            <h2>就业行业统计</h2>
             <Chart14 />
           </div>
         </section>
